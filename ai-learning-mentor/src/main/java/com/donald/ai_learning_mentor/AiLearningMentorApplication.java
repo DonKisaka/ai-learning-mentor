1209973399cb9f180d20d5389803a4ca731722a7
DonaldKisaka <davidkisaka4@gmail.com>
2026-06-04 12:18:33 +0300

    Initial commit

---
+++ ai-learning-mentor/src/main/java/com/donald/ai_learning_mentor/AiLearningMentorApplication.java
@@ -0,0 +1,13 @@
+package com.donald.ai_learning_mentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLearningMentorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiLearningMentorApplication.class, args);
+	}
+
+}
---
+++ ai-learning-mentor/src/test/java/com/donald/ai_learning_mentor/AiLearningMentorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donald.ai_learning_mentor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiLearningMentorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
